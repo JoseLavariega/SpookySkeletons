0ed0bae1c9a5f466b7e80136d819293e0d1d6d21
Jose Lavariega <josea.lavariegago@gmail.com>
2021-11-19 01:16:07 -0500

    non functioning top level with camera

--- final_proj_mydraft/final_proj_mydraft.srcs/sources_1/new/top_level.sv
+++ final_proj_mydraft/final_proj_mydraft.srcs/sources_1/new/top_level.sv
@@ -36,9 +36,141 @@ module top_level(
     // btnc button is user reset
     wire reset;
     debounce db1(.reset_in(reset),.clock_in(clk_65mhz),.noisy_in(btnc),.clean_out(reset));
+
+    logic xclk;
+    logic[1:0] xclk_count;
    
+    logic pclk_buff, pclk_in;
+    logic vsync_buff, vsync_in;
+    logic href_buff, href_in;
+    logic[7:0] pixel_buff, pixel_in;
+    
+    logic [11:0] cam;
+    logic [11:0] frame_buff_out;
+    logic [15:0] output_pixels;
+    logic [15:0] old_output_pixels;
+    logic [12:0] processed_pixels;
+    logic [3:0] red_diff;
+    logic [3:0] green_diff;
+    logic [3:0] blue_diff;
+    logic valid_pixel;
+    logic frame_done_out;
+    
+    logic [16:0] pixel_addr_in;
+    logic [16:0] pixel_addr_out;
+    
+    assign xclk = (xclk_count >2'b01);
+    assign jbclk = xclk;
+    assign jdclk = xclk;
+    
+    assign red_diff = (output_pixels[15:12]>old_output_pixels[15:12])?output_pixels[15:12]-old_output_pixels[15:12]:old_output_pixels[15:12]-output_pixels[15:12];
+    assign green_diff = (output_pixels[10:7]>old_output_pixels[10:7])?output_pixels[10:7]-old_output_pixels[10:7]:old_output_pixels[10:7]-output_pixels[10:7];
+    assign blue_diff = (output_pixels[4:1]>old_output_pixels[4:1])?output_pixels[4:1]-old_output_pixels[4:1]:old_output_pixels[4:1]-output_pixels[4:1];
+    
+    blk_mem_gen_0 jojos_bram(.addra(pixel_addr_in), 
+                             .clka(pclk_in),
+                             .dina(processed_pixels),
+                             .wea(valid_pixel),
+                             .addr(pixel_addr_out),
+                             .clk(clk_65mhz),
+                             .doutb(frame_buff_out));
+    
+    always_ff @(posedge pclk_in)begin
+        if (frame_done_out)begin
+            pixel_addr_in <= 17'b0;  
+        end else if (valid_pixel)begin
+            pixel_addr_in <= pixel_addr_in +1;  
+        end
+    end
+    
+    always_ff @(posedge clk_65mhz) begin
+        pclk_buff <= jb[0];//WAS JB
+        vsync_buff <= jb[1]; //WAS JB
+        href_buff <= jb[2]; //WAS JB
+        pixel_buff <= ja;
+        pclk_in <= pclk_buff;
+        vsync_in <= vsync_buff;
+        href_in <= href_buff;
+        pixel_in <= pixel_buff;
+        old_output_pixels <= output_pixels;
+        xclk_count <= xclk_count + 2'b01;
+        if (sw[3])begin
+            //processed_pixels <= {red_diff<<2, green_diff<<2, blue_diff<<2};
+            processed_pixels <= output_pixels - old_output_pixels;
+        end else if (sw[4]) begin
+            if ((output_pixels[15:12]>4'b1000)&&(output_pixels[10:7]<4'b1000)&&(output_pixels[4:1]<4'b1000))begin
+                processed_pixels <= 12'hF00;
+            end else begin
+                processed_pixels <= 12'h000;
+            end
+        end else if (sw[5]) begin
+            if ((output_pixels[15:12]<4'b1000)&&(output_pixels[10:7]>4'b1000)&&(output_pixels[4:1]<4'b1000))begin
+                processed_pixels <= 12'h0F0;
+            end else begin
+                processed_pixels <= 12'h000;
+            end
+        end else if (sw[6]) begin
+            if ((output_pixels[15:12]<4'b1000)&&(output_pixels[10:7]<4'b1000)&&(output_pixels[4:1]>4'b1000))begin
+                processed_pixels <= 12'h00F;
+            end else begin
+                processed_pixels <= 12'h000;
+            end
+        end else begin
+            processed_pixels = {output_pixels[15:12],output_pixels[10:7],output_pixels[4:1]};
+        end
+            
+    end
+    assign pixel_addr_out = sw[2]?((hcount>>1)+(vcount>>1)*32'd320):hcount+vcount*32'd320;
+    assign cam = sw[2]&&((hcount<640) &&  (vcount<480))?frame_buff_out:~sw[2]&&((hcount<320) &&  (vcount<240))?frame_buff_out:12'h000;
+    
+    ila_0 joes_ila(.clk(clk_65mhz),    .probe0(pixel_in), 
+                                        .probe1(pclk_in), 
+                                        .probe2(vsync_in),
+                                        .probe3(href_in),
+                                        .probe4(jbclk));
+                                        
+   camera_read  my_camera(.p_clock_in(pclk_in),
+                          .vsync_in(vsync_in),
+                          .href_in(href_in),
+                          .p_data_in(pixel_in),
+                          .pixel_data_out(output_pixels),
+                          .pixel_valid_out(valid_pixel),
+                          .frame_done_out(frame_done_out));
+                          
+                          
+     wire border = (hcount==0 | hcount==1023 | vcount==0 | vcount==767 |
+                   hcount == 512 | vcount == 384);
 
+    reg b,hs,vs;
+    always_ff @(posedge clk_65mhz) begin
+      if (sw[1:0] == 2'b01) begin
+         // 1 pixel outline of visible area (white)
+         hs <= hsync;
+         vs <= vsync;
+         b <= blank;
+         rgb <= {12{border}};
+      end else if (sw[1:0] == 2'b10) begin
+         // color bars
+         hs <= hsync;
+         vs <= vsync;
+         b <= blank;
+         rgb <= {{4{hcount[8]}}, {4{hcount[7]}}, {4{hcount[6]}}} ;
+      end else begin
+         // default:
+         //rgb <= pixel;
+         rgb <= cam;
+      end
+    end
 
+//    assign rgb = sw[0] ? {12{border}} : pixel ; //{{4{hcount[7]}}, {4{hcount[6]}}, {4{hcount[5]}}};
+
+    // the following lines are required for the Nexys4 VGA circuit - do not change
+    assign vga_r = ~b ? rgb[11:8]: 0;
+    assign vga_g = ~b ? rgb[7:4] : 0;
+    assign vga_b = ~b ? rgb[3:0] : 0;
+
+    assign vga_hs = ~hs;
+    assign vga_vs = ~vs;
     
 endmodule
 
